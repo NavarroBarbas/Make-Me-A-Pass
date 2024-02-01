6963b3f0180954a8eaa110be24a55671aac7d9e9
Javier Navarro <javiernavarrodieguez@gmail.com>
2024-02-01 12:41:13 +0100

    Guardar Contraseña generada convertido a PDO

--- php/guardarpass.php
+++ php/guardarpass.php
@@ -7,7 +7,25 @@
         $randompass = $_POST['password'];
         $email = $_SESSION['email'];
 
-        $sqlid = 'SELECT user_id FROM usuarios WHERE email = "' . $email . '"';
+        $pdo = new Conexion();
+        $resid = $pdo->prepare('SELECT user_id FROM usuarios WHERE email =:email');
+        $resid->bindValue(':email', $email);
+        $resid->execute();
+
+        $resultado = $resid->fetchAll();
+        foreach($resultado as $row) {
+            $userid = $row['user_id'];
+        }
+
+        $insert = $pdo->prepare('INSERT INTO saved_passwords (generated_pass, nombre_pass, user_id)
+            VALUES (:randompass, :nombrepass, :userid)');
+        $insert->bindValue(':randompass', $randompass);
+        $insert->bindValue(':nombrepass', $nombrepass);
+        $insert->bindValue(':userid', $userid);
+        $insert->execute();
+        echo "Insert realizado";
+
+        /*$sqlid = 'SELECT user_id FROM usuarios WHERE email = "' . $email . '"';
         $resid = mysqli_query($conexion, $sqlid);
         $column = $resid -> fetch_assoc();
         $userid = $column['user_id'];
@@ -16,7 +34,7 @@
             VALUES ("' . $randompass . '", "' . $nombrepass . '", ' . $userid . ')';
         $resins = mysqli_query($conexion, $sqlinsert);
         
-        echo "Insert realizado";
+        echo "Insert realizado";*/
     } else {
         echo "Error: Método no permitido.";
     }
